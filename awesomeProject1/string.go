4679b3ed95db168d52aa1cea471216967dbe2d39
imamsholehudin <sholehudinimam@gmail.com>
2020-05-12 23:42:04 +0700

    golang first

---
+++ awesomeProject1/string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Belajar Golang")
+	fmt.Println("Belajar"+ " "+"Golang")
+	fmt.Println(len("Belajar Imam"))
+	fmt.Println("belajar"[0])
+	fmt.Println("belajar"[0:4])
+	fmt.Println("belajar"[0:1])
+	fmt.Println("belajar"[2:])
+	fmt.Println("belajar"[:2])
+}
---
+++ awesomeProject1/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var hello string = "Hello World";
+	fmt.Println(hello)
+	fmt.Println(hello + "Golang")
+	fmt.Println(hello)
+	var angka int = 45
+	fmt.Println(angka)
+	fmt.Println(angka+7)
+	fmt.Println(angka+angka)
+
+	namaLengkap := "Imam Sholehudin"
+	fmt.Println(namaLengkap)
+	angka1 := 75
+	fmt.Println(angka1)
+
+}
